50c190271052433558e8bb57e0b6c68004be2c2b
Federico Frenguelli <synasius@gmail.com>
2014-05-21 09:35:46 +0200

    Add browsable api login urls

--- timetracker-backend/timetracker/urls.py
+++ timetracker-backend/timetracker/urls.py
@@ -11,5 +11,7 @@ apiurls = patterns(
 urlpatterns = patterns(
     '',
     url(r'^admin/', include(admin.site.urls)),
+
     url(r'^api/', include(apiurls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 )
