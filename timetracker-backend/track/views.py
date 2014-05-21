60d7b703923efe281355e137e6121a3b4dfc21af
Federico Frenguelli <synasius@gmail.com>
2014-05-21 09:35:13 +0200

    TimeEntryViewSet returns only current user's entries and set entry.user to request.user on save

--- timetracker-backend/track/views.py
+++ timetracker-backend/track/views.py
@@ -1,6 +1,7 @@
 from rest_framework import viewsets
 
 from .models import Activity, TimeEntry
+from .serializers import TimeEntrySerializer
 
 
 class ActivityViewSet(viewsets.ModelViewSet):
@@ -9,3 +10,10 @@ class ActivityViewSet(viewsets.ModelViewSet):
 
 class TimeEntryViewSet(viewsets.ModelViewSet):
     model = TimeEntry
+    serializer_class = TimeEntrySerializer
+
+    def get_queryset(self):
+        return self.request.user.entries.all()
+
+    def pre_save(self, obj):
+        obj.user = self.request.user
